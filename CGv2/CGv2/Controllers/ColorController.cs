7c2ddbf384833f2ad615bcbcc03fc6ce26a630b4
maxpopovych <maxpopovych123@gmail.com>
2020-12-12 16:07:37 +0200

    fix

---
+++ CGv2/CGv2/Controllers/ColorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CGv2.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Media.Imaging;
+
+namespace CGv2.Controllers
+{
+    [Route("ColorModelM")]
+    public class ColorModelMController : Controller
+    {
+        // GET: ColorModelM
+        [HttpGet]
+        [Route("/")]
+        public ActionResult Index()
+        {
+            //Draw();
+            return View();
+        }
+        /*public ActionResult Index(int c,int k, double zoom)
+        {
+            Draw(c,k,zoom);
+            return View();
+        }*/
+
+    }
+}
