73bb0f5fa21ba07f2d5bff4329f5a6b1384bf8af
maxpopovych <maxpopovych123@gmail.com>
2021-01-05 19:20:25 +0200

    .

---
+++ CGv2/CGv2/Controllers/AffinController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CGv2.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Media.Imaging;
+
+namespace CGv2.Controllers
+{
+    [Route("Affin")]
+    public class AffinController : Controller
+    {
+        [HttpGet]
+        [Route("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
